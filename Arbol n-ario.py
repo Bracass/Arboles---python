516ed6f9228dc7e19b10243e85ba1327c8d22ac8
sebasdu98 <42306260+sebasdu98@users.noreply.github.com>
2018-10-03 17:46:10 -0500

    Update Arbol n-ario.py

--- Arbol n-ario.py
+++ Arbol n-ario.py
@@ -18,11 +18,7 @@ def buscar_en_hijos(lista, valor):
 		return buscar_n(lista[0], valor) or buscar_en_hijos(lista[1:], valor)
 	
 
-arbol = NodoN(10,[NodoN(20,[NodoN(40), NodoN(44)]),
-				  NodoN(52,[NodoN(150)]),
-				  NodoN(50),
-				  NodoN(100,[NodoN(250),NodoN(500),NodoN(1000)])])
 
-print (buscar_n(arbol, 10))
-print (buscar_n(arbol, 1001))
-#print (buscar_en_hijos([],20))
+a1= NodoN(2,[NodoN(4,[NodoN(12,[]),NodoN(24,[]),NodoN(40,[])]),NodoN(8,[NodoN(16,[]),NodoN(32,[])]),NodoN(5,[NodoN(25,[]),NodoN(50,[])])])
+print (buscar_n(a1, 10))
+print (buscar_n(a1, 50))
